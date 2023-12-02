00535f617aa7dc9575abc748aa5a97de850eb6be
sank4lp55 <sankalpsrivastav55@gmail.com>
2023-12-02 11:11:00 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Coal Tracking App',
+      title: 'Coal Tracking App :)',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
